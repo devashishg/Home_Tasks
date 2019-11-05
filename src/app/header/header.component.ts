6caf64c6e42c063ceee238082a920b55c52bb430
Devashish <Devashish_gupta@epam.com>
2019-11-05 19:40:09 +0530

    headerFooter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+  public TitlePage:String;
+  constructor() { 
+    this.TitlePage  = 'Source Name';;
+  }
+  
   ngOnInit() {
   }
 

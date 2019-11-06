b55f73bb9ae1feb9f3597b5484e15ecb81ce36af
Devashish <Devashish_gupta@epam.com>
2019-11-06 19:40:13 +0530

    SecondaryNavBarAdded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FeedAreaComponent } from './feed-area/feed-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FeedAreaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feed-area/feed-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-area',
+  templateUrl: './feed-area.component.html',
+  styleUrls: ['./feed-area.component.css']
+})
+export class FeedAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

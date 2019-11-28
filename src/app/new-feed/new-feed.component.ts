bdb157730d20fcb5bdb6bbfd48890bf90b61aa0a
Devashish <Devashish_gupta@epam.com>
2019-11-28 14:41:59 +0530

    resetResolved

--- src/app/new-feed/new-feed.component.ts
+++ src/app/new-feed/new-feed.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FeedAreaComponent } from '../feed-area/feed-area.component';
 import { LoginServiceService } from '../Service/login-service.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-new-feed',
@@ -9,39 +11,53 @@ import { LoginServiceService } from '../Service/login-service.service';
   styleUrls: ['./new-feed.component.css']
 })
 export class NewFeedComponent implements OnInit {
-  public user: String;
+  public user: string;
+  public userDummy: string;
   public submitted: boolean;
-  constructor(private route: Router, private logInServices: LoginServiceService) {
-    this.submitted = false;
-    this.user = logInServices.getUser();;
-  }
 
+  constructor(private route: Router, public logInServices: LoginServiceService) {
+    this.submitted = false;
+  }
 
   ngOnInit() {
     if (!this.logInServices.getStatus()) {
       this.route.navigate(['/login']);
     }
+    
+    this.user = this.logInServices.getUser();
+    this.userDummy = this.user;
   }
 
   submissionNewFeed = (obj) => {
-    console.log(obj);
     this.submitted = true;
     if ((FeedAreaComponent.MyArticles && FeedAreaComponent.MyArticles[0].author === 'Error') || !FeedAreaComponent.MyArticles) {
       FeedAreaComponent.MyArticles = new Array();
     }
 
     if (obj.form.status === "VALID") {
-      window.alert(`Article Saved:
-
-      ${JSON.stringify(obj.value)} `);
+      window.alert(`Article Saved !! `);
+      console.log(`${JSON.stringify(obj.value)}`)
       FeedAreaComponent.MyArticles.push(obj.value);
+      this.resetForm(obj);
     } else {
-      window.alert(`InValid Input`);
+      window.alert(`inValid Input`);
     }
   }
 
   navigateBack() {
     this.route.navigate(['/NewsFeeds']);
+  }
+
+  resetForm = (obj)=>{
+    obj.reset({
+      'title': '',
+      'Author' : this.user,
+      'publishedAt': '',
+      'ShortDesc': '',
+      'url': '',
+      'description': '',
+      'urlToImage': '',
+    });
   }
 
 }
